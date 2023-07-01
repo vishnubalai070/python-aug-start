f24ea7c4c24f07b92a3a29fcf9f0bf7dd1d2da51
vishnubalai070 <111786064+vishnubalai070@users.noreply.github.com>
2023-07-01 15:45:16 +0530

    Add files via upload

---
+++ june2023 1st.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+
+a = 10
+b = 109
+c=(a+b)
+print(c)
+
+
+# In[19]:
+
+
+a=25.4
+b=67.8
+c=(a-b)
+print(c)
+
+
+# In[23]:
+
+
+a=10
+b=22
+print(a>b )
+
+
+# In[24]:
+
+
+x=22+5j
+y=26+5j
+print(x*y)
+
+
+# In[27]:
+
+
+(22+5j)*(26+5j)
+
+
+# In[38]:
+
+
+print('hate''the''world')
+
+
+# In[39]:
+
+
+print('hate','the','world')
+
+
+# In[52]:
+
+
+print('hate''_the''_world')
+
+
+# In[ ]:
+
+
+
+
