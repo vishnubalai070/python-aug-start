0f5546017e9cd49bba1a9bf12ceae426cd72db62
vishnu <111786064+vishnubalai070@users.noreply.github.com>
2023-07-08 12:22:19 +0530

    Add files via upload

---
+++ data types .py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+data types are the classification or categorization of data items.
+it presesents the kind of value that tells what operation can be performed on the particular data
+
+there are 8 typs of data types
+text type (str) example
+we can store the albhatic values are called text type data
+x=(hello)
+
+numeric types 
+numeric type means we can store or any nymers add,mul,divi using mathamatic opreation used data are called numeric type
+thera are 3 types of data
+
+int ,float ,complex
+int x=(1)
+float x=(22.5)
+complex x=(22+3j)
+
+sequence type
+sequence type means we can store the one person hole data in list form is called sequence type 
+there are 3 type
+
+list {name : vishnu , age : 24}
+is muttable we can change the data
+
+tuple 
+is immutable we can never change the value
+a=('vishnu',420,20.5)
+a[0]
+range
+means used for start to stop to read the opration function used 
+for i in range(1,10):
+print(i, end="")
+
+mapping type 
+know the data when stored data location to fund the mappinng data
+x=10
+print(id(x))
+
+boolean type 
+we can write the programing for one opration functon on thet program true or false find to use this boolean type data function
+
+x=(11)
+y=(22)
+print(x>y)
+
+
+# In[7]:
+
+
+a=('vishnu',420,20.5)
+a[0]
+
+
+# In[34]:
+
+
+x=(11)
+y=(22)
+print(x>y)
+
+
+# In[35]:
+
+
+x=10
+print(id(x))
+
+
+# In[37]:
+
+
+x=(11)
+y=(22)
+print(x>y)
+print(x<y)
+
+
+# In[ ]:
+
+
+
+
