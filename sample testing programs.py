aa3ef351572696730bff5e0c1c778e29fe20d9db
vishnu <111786064+vishnubalai070@users.noreply.github.com>
2023-07-09 12:24:22 +0530

    Add files via upload

---
+++ sample testing programs.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
+#tuple
+a=('vishnu',420,20.5)
+print(a[0])
+
+
+# In[5]:
+
+
+#range
+for i in range(1,10):
+    print(i, end="")
+
+
+# In[6]:
+
+
+#indentify operator
+x=10
+print(id(x))
+
+
+# In[10]:
+
+
+#boolean opreator
+x=(11)
+y=(22)
+print(x>y)
+print(x<y)
+print(x==y)
+
+
+# In[12]:
+
+
+#add,mul,div,sub in int,float,complax
+a=22 #int
+b=30 #int
+c=22.50 #float
+d=44.35 #float
+e=33.5j #complax
+print(a+b) 
+print(c-d) 
+print(a*c)
+print(a+e)
+
+
+# In[28]:
+
+
+a=("name:vishnu", "age:25" )
+print (a[1])
+print(type(a))
+print(id(a))
+print(a)
+
+
+# In[ ]:
+
+
+
+
