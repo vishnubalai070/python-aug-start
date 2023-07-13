72ace599f77b96523af1993a1cdf266c72c4bb84
vishnu <111786064+vishnubalai070@users.noreply.github.com>
2023-07-13 12:21:07 +0530

    Add files via upload

---
+++ list,tuple,set,dictonary seminor (1).py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[30]:
+
+
+#list  - [] square braces ,mutabable
+#tuple - () round braces , immuatable
+#set   - set() the set is a keyword, immutable
+#dict  - {} curley braces 
+
+list1=["python","june","2023",44,33,12.50]
+print(list1,type(list1))
+
+tuple1 = ("python","june","2023",44,33,12.50)
+print(tuple1,type(tuple1))
+
+set1 = set(["python","june","2023",44,33,12.50])
+print(set1,type(set1))
+
+dict1 = {
+    1: "python",
+    2: "june",
+    3: "2013",
+}
+print(dict1,type(dict1))
+
+list1=["python","june","2023",44,33,12.50]
+list1[0]="welcome"# we can add and change the value is called mutable
+print(list1)
+
+tuple1 = ("python","june","2023",44,33,12.50)
+tuple1[0]="welcome"
+print(tuple1)
+
+
+# In[25]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
