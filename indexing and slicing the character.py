6d25b252d419f9fcbe9b5a98d100eb5e071a24aa
vishnu <111786064+vishnubalai070@users.noreply.github.com>
2023-07-29 14:44:41 +0530

    Add files via upload

---
+++ indexing and slicing the character.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+var=("i_love_india")
+print(var[3])
+print(var[4])
+print(var[-5])
+print(var[-9])
+print(var[-10])
+print(var[-3])
+print(var[5])
+
+
+# In[3]:
+
+
+a=("pythonprogrammingtesting")
+print(a[3:8])
+print(a[5:-5])
+print(a[-12:-7])
+print(a[-10:15])
+print(a[:6])
+print(a[:-7])
+print(a[5:])
+print(a[-10:])
+print(a[:])
+print(a[4:100])
+
+
+# In[10]:
+
+
+#write a program to calculate area of circle with radius 7 area=PI*r**2
+"the value of pi is 3.14"
+PI=3.14 
+"so we can assingn and store the value into the variable of PI" 
+"and asign the value of radious is 7" 
+radius=7
+"the forumla of the findnig value of area=PI*r**2"
+area = PI * radius * radius
+"now we print the passing of arugment to use the print fuction to"
+print(area)
+
+
+# In[11]:
+
+
+#induxing and slicing
+#0    1    2    3    4    5     6    7    8    9   10   11   12   13   14   15   16   17   18   19   20   21  w    a    k    e    _    u     p    _    t    o    _   t    h    e    _    r    e    a    l    i    t     y 
+# -22 -21  -20  -19  -18  -17  -16  -15  -14  -13  -12  -11  -10   -9   -8   -7   -6   -5   -4   -3    -2   -1
+a=("wake_up_to reality")
+print(a[3:8])
+print(a[5:-5])
+print(a[-12:-7])
+print(a[-10:15])
+print(a[:6])
+print(a[:-7])
+print(a[5:])
+print(a[-10:])
+print(a[:])
+print(a[4:100])
+
+
+# In[ ]:
+
+
+
+
