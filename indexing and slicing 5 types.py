01ee3b3c5b9280f0498d668c9d5357b162fce784
vishnu <111786064+vishnubalai070@users.noreply.github.com>
2023-08-14 02:07:59 +0530

    Add files via upload

---
+++ indexing and slicing 5 types.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+#type1
+a="hukum_tiger_ha_hukum"
+print(a[0:-1])
+print(a[1:-1])
+print(a[2:12])
+print(a[3:11])
+print(a[4:2])
+print(a[5:-2])
+print(a[6:-3])
+print(a[7:-2])
+print(a[8:-2])
+print(a[9:-2])
+print(a[10:-7])
+print(a[11:-8])
+print(a[12:-5])
+print(a[5:-2])
+print(a[6:9])
+print(a[0:4])
+print(a[6:9])
+print(a[9:-1])
+print(a[5:-7])
+print(a[8:-4])
+
+
+# In[5]:
+
+
+#type2
+a="artham_arintha_raja"
+print(a[0:-1:16])
+print(a[1:-1:1])
+print(a[2:12:15])
+print(a[3:11:2])
+print(a[4:2:14])
+print(a[5:-2:3])
+print(a[6:-4:11])
+print(a[7:-5:5])
+print(a[8:-6:1])
+print(a[9:-2:3])
+print(a[10:-7:7])
+print(a[11:-8:4])
+print(a[12:-5:8])
+print(a[5:-2:9])
+print(a[6:9:10])
+print(a[0:4:2])
+print(a[6:9:5])
+print(a[9:-1:5])
+print(a[5:-7:4])
+print(a[8:-4:2])
+
+
+# In[1]:
+
+
+#type3
+a="asr_traders_virudhunagar"
+print(a[4:8:2]) 
+print(a[8:-8:-3]) 
+print(a[0:-8:1]) 
+print(a[-6:6:1]) 
+print(a[:6:3])
+print(a[4:8:2])
+print(a[8:-8:5])
+print(a[0:-8:1])
+print(a[-6:6:1])
+print(a[:6:3])
+print(a[:-9:-5])
+print(a[2::4])
+print(a[-2:4:4])
+
+
+# In[6]:
+
+
+#type4
+a="if_you_just_see_me_not_know_me"
+print(a[6::3])
+print(a[6::-3])
+print(a[:8:3])
+print(a[:8:-3])
+print(a[2::5])
+print(a[10::5])
+print(a[:6:1])
+print(a[:7:-2])
+print(a[1::2])
+print(a[9::-9])
+print(a[3::-6])
+
+
+# In[5]:
+
+
+#type5
+a="dont_worry_about_silly things"
+print(a[::1])
+print(a[::-1])
+print(a[::2])
+print(a[::-2])
+print(a[::-5])
+print(a[::-9])
+print(a[::6])
+print(a[::-3])
+print(a[::7])
+print(a[::-12])
+print(a[::-19])
+print(a[::17])
+print(a[::19])
+print(a[::-8])
+print(a[::9])
+print(a[::22])
+print(a[::19])
+print(a[::14])
+print(a[::11])
+print(a[::-11])
+
+
+# In[64]:
+
+
+#string nethod
+a="if you JUst sEe Me nOt know me"
+b=['whats','up','haters']
+c="hi","whatsup"
+print(a.capitalize())
+print(a.lower())
+print(a.upper())
+print(a.title())
+print(a.count("e"))
+print(a.strip())
+print(''.join(b))
+print(' '.join(b))
+print(','.join(b))
+print("".join(a))
+print(a.rstrip())
+print(a.())
+print(c.replace("hi" , "hello"))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
